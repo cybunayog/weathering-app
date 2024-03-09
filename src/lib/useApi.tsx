b89c1248c152719c1318f5cc1b8caee73ce8602e
Cy Bunayog <cybunayog@gmail.com>
2024-03-09 01:52:06 -0800

    hotfix: url

--- src/lib/useApi.tsx
+++ src/lib/useApi.tsx
@@ -13,11 +13,14 @@ export function useApi(url: string, method: Method) {
     (async () => {
       setIsLoading(true);
 
+      console.log(url);
+
       try {
         const response: AxiosResponse = await axios({ url, method });
         setData(response.data);
       } catch (error) {
         // TODO: figure out error object
+        console.log(error);
         const errorMessage = error.message;
         setError(errorMessage);
       }
@@ -43,7 +46,7 @@ export function useOpenWeatherApi(
     suffix = `lat=${coords.lat}&lon=${coords.long}`;
   }
   return useApi(
-    `${process.env.EXPO_PUBLIC_OPEN_WEATHER_URL}/${path}&units=${isImperial ? "imperial" : "metric"}&appid=${process.env.EXPO_PUBLIC_OPEN_WEATHER_KEY}&${suffix}`,
+    `${process.env.EXPO_PUBLIC_OPEN_WEATHER_URL}/${path}?${suffix}&units=${isImperial ? "imperial" : "metric"}&appid=${process.env.EXPO_PUBLIC_OPEN_WEATHER_KEY}`,
     method,
   );
 }
